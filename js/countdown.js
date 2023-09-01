60f346f1ee623c5767df5682680da01a8f594c17
Erik J Hedl <erik.hedl@colorado.edu>
2023-09-01 17:00:10 -0600

    changing date format

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,6 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 30, 2024 17:01:00").getTime();
+//var countDownDate = new Date("Apr 30, 2024 17:01:00").getTime();
+var countDownDate = new Date(2024, 04, 30, 17, 01, 00).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
